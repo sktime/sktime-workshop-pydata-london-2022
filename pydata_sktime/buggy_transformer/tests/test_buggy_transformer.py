b8d49182b4bdb82224a790bb0ce65e377791f670
ltsaprounis <leonidas.tsap@gmail.com>
2022-06-13 00:20:59 +0100

    added tests for all modules and scitype/mtype section in notebooks

---
+++ pydata_sktime/buggy_transformer/tests/test_buggy_transformer.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""Simple test suite for estimators."""
+
+import pytest
+from sktime.utils.estimator_checks import check_estimator
+
+from pydata_sktime.buggy_transformer import MinMaxScaler
+
+MY_ESTIMATORS = [MinMaxScaler]
+
+
+@pytest.mark.parametrize("estimator_class", MY_ESTIMATORS)
+def test_my_estimators(estimator_class):
+    """Runs sktime check_estimator test suite on the estimator_class."""
+    check_estimator(estimator_class, return_exceptions=False)
